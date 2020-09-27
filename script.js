208e860eb60f1d05654272bc2c2b9f6fdd002be5
Curious Garlic <curiousgarlic@gmail.com>
2020-09-27 08:39:45 +0300

    change links to empty

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ window.addEventListener('DOMContentLoaded', () => {
  *  Skills
  *******/
 const skillsDiv = document.querySelector('#skills-list');
-const skills = [['JavaScript', 2], ['HTML', 2], ['CSS', 2], ['Sass', 1], ['Node', 1], ['Express', 1], ['Angular', 1], ['React', 1], ['jQuery', 2],['D3',1], ['MongoDB', 1], ['Git', 1]];
+const skills = [['JavaScript', 3], ['HTML', 3], ['CSS', 3], ['Sass', 2], ['Node', 2], ['Express', 2], ['React', 1], ['MongoDB', 1], ['Git', 1], ['Figma', 2]];
 const fullStar = '&#9733;';
 const emptyStar = '&#9734;'
 
@@ -105,7 +105,7 @@ const certs = {
             name: 'Responsive Web Design',
             image: 'Responsive_Web_Design.png',
             issued: 'Issued June 25, 2018',
-            link: 'https://www.freecodecamp.org/certification/nazarja/legacy-front-end'
+            link: '#'
         },
         {
             educator: 'FreeCodeCamp',
@@ -113,7 +113,7 @@ const certs = {
             name: 'APIs and Microservices',
             image: 'APIs_and_Microservices.png',
             issued: 'Issued June 14, 2018',
-            link: 'https://www.freecodecamp.org/certification/nazarja/apis-and-microservices'
+            link: '#'
         },
         {
             educator: 'Udacity',
@@ -121,7 +121,7 @@ const certs = {
             name: 'Mobile Web Specialist Nanodegree',
             image: 'Mobile_Web_Specialist.png',
             issued: 'Issued June 11, 2018',
-            link: 'https://graduation.udacity.com/confirm/YDLGKETD'
+            link: '#'
         },
         {
             educator: 'FreeCodeCamp',
@@ -129,7 +129,7 @@ const certs = {
             name: 'Algorithms and Data Structures',
             image: 'Algorithms_and_Data_Structures.png',
             issued: 'Issued May 31, 2018',
-            link: 'https://www.freecodecamp.org/certification/nazarja/javascript-algorithms-and-data-structures'
+            link: '#'
         },
         {
             educator: 'FreeCodeCamp',
@@ -137,7 +137,7 @@ const certs = {
             name: 'Legacy Data Visualization Program',
             image: 'Legacy_Data_Visualization.png',
             issued: 'Issued March 11, 2018',
-            link: 'https://www.freecodecamp.org/certification/nazarja/legacy-data-visualization'
+            link: '#'
         },
         {
             educator: 'FreeCodeCamp',
@@ -145,7 +145,7 @@ const certs = {
             name: 'Legacy Front End Development Program',
             image: 'Legacy_Front_End_Development.png',
             issued: 'Issued July 13, 2017',
-            link: 'https://www.freecodecamp.org/certification/nazarja/legacy-front-end'
+            link: '#'
         },
         {
             educator: 'Adobe',
@@ -153,7 +153,7 @@ const certs = {
             name: 'Adobe Certified Associate in Visual Communication Using Adobe Photoshop',
             image: 'Photoshop_ACA.png',
             issued: 'Issued June 16, 2016',
-            link: 'https://www.youracclaim.com/badges/6e629d04-8be0-4d9c-b1d6-a28fe195c8c6/linked_in_profile?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BXAX6P7DPTmyZWawJKp2mRg%3D%3D'
+            link: '#'
         }
     ]
 };
@@ -232,178 +232,178 @@ const projects = {
     Featured: [
         {
             name: 'Build a JS & CSS Photo Editor',
-            image: 'http://seanmurphy.eu/images/projects/Featured/photo_editor.png',
-            link: 'http://seanmurphy.eu/photo-editor',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript', 'CSS', 'HTML']
         },
         {
             name: 'Build the Game "Breakout" ',
-            image: 'http://seanmurphy.eu/images/projects/Featured/breakout_game.png',
-            link: 'http://seanmurphy.eu/breakout-game',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript', 'CSS', 'HTML']
         }, 
         {
             name: 'Visualize Global Data with a Heatmap',
-            image: 'http://seanmurphy.eu/images/projects/DataVisualization/heatmap.png',
-            link: 'http://seanmurphy.eu/heatmap',
+            image: '#',
+            link: '#',
             'technologies': ['D3', 'JSON']
         },
         {
             name: 'Build a Recipe Box',
-            image: 'http://seanmurphy.eu/images/projects/ReactJS/recipe_box.png',
-            link: 'http://seanmurphy.eu/recipe-box',
+            image: '#',
+            link: '#',
             'technologies': ['React', 'Node', 'Local Storage']
         },
         {
             name: 'Build a Weather App',
-            image: 'http://seanmurphy.eu/images/projects/FrontEndLibraries/weather_app.png',
-            link: 'http://seanmurphy.eu/weather-app',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript', 'JSON', 'Web API']
         },
         {
             name: 'Image Search Abstraction Layer',
-            image: 'http://seanmurphy.eu/images/projects/APIsAndMicroservices/image_search_abstraction_layer.png',
-            link: 'https://sm-image-search-abstraction-layer.glitch.me/',
+            image: '#',
+            link: '#',
             'technologies': ['Node', 'Express', 'MongoDB', 'Web API']
         }
     ],
     ResponsiveWebDesign: [
         {
             name: 'Build a Product Landing Page',
-            image: 'http://seanmurphy.eu/images/projects/ResponsiveWebDesign/product_landing_page.png',
-            link: 'http://seanmurphy.eu/product-landing-page',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript', 'CSS', 'HTML']
         },
         {
             name: 'Build a Product Landing Page',
-            image: 'http://seanmurphy.eu/images/projects/ResponsiveWebDesign/survey_form.png',
-            link: 'http://seanmurphy.eu/survey-form',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript', 'CSS', 'HTML']
         },
         {
             name: 'Build a Technical Documentation Page',
-            image: 'http://seanmurphy.eu/images/projects/ResponsiveWebDesign/technical_documentation_page.png',
-            link: 'http://seanmurphy.eu/technical-documentation-page',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript', 'CSS', 'HTML']
         },
         {
             name: 'Build a Tribute Page',
-            image: 'http://seanmurphy.eu/images/projects/ResponsiveWebDesign/tribute_page.png',
-            link: 'http://seanmurphy.eu/tribute-page',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript', 'CSS', 'HTML']
         }
     ],
     FrontEndLibraries: [
         {
             name: 'Build a Random Quote Machine',
-            image: 'http://seanmurphy.eu/images/projects/FrontEndLibraries/random_quote_machine.png',
-            link: 'http://seanmurphy.eu/random-quote-machine',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript', 'JSON', 'Web API']
         },
         {
             name: 'Build a Markdown Previewer',
-            image: 'http://seanmurphy.eu/images/projects/FrontEndLibraries/markdown_previewer.png',
-            link: 'http://seanmurphy.eu/markdown-previewer',
+            image: '#',
+            link: '#',
             'technologies': ['React', 'Markdown']
         },
         {
             name: 'Build a JavaScript Calculator',
-            image: 'http://seanmurphy.eu/images/projects/FrontEndLibraries/calculator.png',
-            link: 'http://seanmurphy.eu/calculator',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript']
         },
         {
             name: 'Build a Pomodoro Clock',
-            image: 'http://seanmurphy.eu/images/projects/FrontEndLibraries/pomodoro_clock.png',
-            link: 'http://seanmurphy.eu/pomodoro-clock',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript']
         },
         {
             name: 'Build a Weather App',
-            image: 'http://seanmurphy.eu/images/projects/FrontEndLibraries/weather_app.png',
-            link: 'http://seanmurphy.eu/weather-app',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript', 'JSON', 'Web API']
         },
         {
             name: 'Build a Simon Game',
-            image: 'http://seanmurphy.eu/images/projects/FrontEndLibraries/simon_game.png',
-            link: 'http://seanmurphy.eu/',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript']
         },
         {
             name: 'Build a Wikipedia Viewer',
-            image: 'http://seanmurphy.eu/images/projects/FrontEndLibraries/wikipedia_viewer.png',
-            link: 'http://seanmurphy.eu/',
+            image: '#',
+            link: '#',
             'technologies': ['Javascript', 'JSON', 'Web API']
         }
     ],
     DataVisualization: [
         {
             name: 'Map Data with a Bar Chart',
-            image: 'http://seanmurphy.eu/images/projects/DataVisualization/bar_chart.png',
-            link: 'http://seanmurphy.eu/bar-chart',
+            image: '#',
+            link: '#',
             'technologies': ['D3', 'JSON']
         },
         {
             name: 'National Contiguity - Force Directed Graph',
-            image: 'http://seanmurphy.eu/images/projects/DataVisualization/force_directed_graph.png',
-            link: 'http://seanmurphy.eu/force-directed-graph',
+            image: '#',
+            link: '#',
             'technologies': ['D3', 'JSON']
         },
         {
             name: 'Visualize Global Data with a Heatmap',
-            image: 'http://seanmurphy.eu/images/projects/DataVisualization/heatmap.png',
-            link: 'http://seanmurphy.eu/heatmap',
+            image: '#',
+            link: '#',
             'technologies': ['D3', 'JSON']
         },
         {
             name: 'Map Meteroite Data Across the Globe',
-            image: 'http://seanmurphy.eu/images/projects/DataVisualization/map_global_data.png',
-            link: 'http://seanmurphy.eu/map-global-data',
+            image: '#',
+            link: '#',
             'technologies': ['D3', 'JSON']
         },
         {
             name: 'Visualize Data with a Scatterpot Graph',
-            image: 'http://seanmurphy.eu/images/projects/DataVisualization/scatterplot_graph.png',
-            link: 'http://seanmurphy.eu/scatterplot-graph',
+            image: '#',
+            link: '#',
             'technologies': ['D3', 'JSON']
         }
     ],
     ApisAndMicroservices: [
         {
             name: 'Build a Timestamp Microservice',
-            image: 'http://seanmurphy.eu/images/projects/APIsAndMicroservices/timestamp_microservice.png',
-            link: 'https://sm-timestamp-microservice.glitch.me/',
+            image: '#',
+            link: '#',
             'technologies': ['Node', 'Express']
         },
         {
             name: 'Build a Request Header Parser Microservice',
-            image: 'http://seanmurphy.eu/images/projects/APIsAndMicroservices/request_header_parser.png',
-            link: 'https://sm-request-header-parser-microservice.glitch.me/',
+            image: '#',
+            link: '#',
             'technologies': ['Node', 'Express']
         },
         {
             name: 'Build a URL Shortener Microservice',
-            image: 'http://seanmurphy.eu/images/projects/APIsAndMicroservices/url_shortener.png',
-            link: 'https://sm-url-microservice.glitch.me/',
+            image: '#',
+            link: '#',
             'technologies': ['Node', 'Express', 'MongoDB', 'url-valid']
         },
         {
             name: 'Image Search Abstraction Layer',
-            image: 'http://seanmurphy.eu/images/projects/APIsAndMicroservices/image_search_abstraction_layer.png',
-            link: 'https://sm-image-search-abstraction-layer.glitch.me/',
+            image: '#',
+            link: '#',
             'technologies': ['Node', 'Express', 'MongoDB', 'Web API']
         },
         {
             name: 'File Metadata Microservice',
-            image: 'http://seanmurphy.eu/images/projects/APIsAndMicroservices/file_metadata_microservice.png',
-            link: 'https://sm-file-metadata.glitch.me/',
+            image: '#',
+            link: '#',
             'technologies': ['Node', 'Express', 'MongoDB', 'multer']
         },
         {
             name: 'Build an Exercise Tracker',
-            image: 'http://seanmurphy.eu/images/projects/APIsAndMicroservices/exercise_tracker.png',
-            link: 'https://sm-exercise-tracker.glitch.me/',
+            image: '#',
+            link: '#',
             'technologies': ['Node', 'Express', 'MongoDB', 'Mongoose']
         }
     ],
@@ -411,32 +411,32 @@ const projects = {
     ReactJS: [
         {
             name: 'Build a Github Style Markdown Previewer',
-            image: 'http://seanmurphy.eu/images/projects/ReactJS/markdown_previewer.png',
-            link: 'http://seanmurphy.eu/markdown-previewer',
+            image: '#',
+            link: '#',
             'technologies': ['React', 'Node', 'Markdown']
         },
         {
             name: 'Build a Camper Leaderboard',
-            image: 'http://seanmurphy.eu/images/projects/ReactJS/camper_leaderboard.png',
-            link: 'http://seanmurphy.eu/camper-leaderboard',
+            image: '#',
+            link: '#',
             'technologies': ['React', 'Node', 'AJAX']
         },
         {
             name: 'Build Conway\'s Game of Life',
-            image: 'http://seanmurphy.eu/images/projects/ReactJS/conways_game_of_life.png',
-            link: 'http://seanmurphy.eu/game-of-life',
+            image: '#',
+            link: '#',
             'technologies': ['React', 'Node']
         },
         {
             name: 'Build a Recipe Box',
-            image: 'http://seanmurphy.eu/images/projects/ReactJS/recipe_box.png',
-            link: 'http://seanmurphy.eu/recipe-box',
+            image: '#',
+            link: '#',
             'technologies': ['React', 'Node', 'Local Storage']
         },
         {
             name: 'Build a Rougelike Dungeon Crawler Game',
-            image: 'http://seanmurphy.eu/images/projects/ReactJS/roguelike_dungeon_crawler.png',
-            link: 'http://seanmurphy.eu/roguelike-dungeon-crawler',
+            image: '#',
+            link: '#',
             'technologies': ['React', 'Node', 'Phaser']
         }
     ]
